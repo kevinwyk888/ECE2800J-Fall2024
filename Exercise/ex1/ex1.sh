a2be4e375de2980151a056c5ccdefff973e2ce46
kevin_wyk <kevin_wyk@sjtu.edu.cn>
2024-09-29 11:10:45 +0800

    feat: ex1

--- Exercise/ex1/ex1.sh
+++ Exercise/ex1/ex1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -r Coursework
+
 mkdir Coursework
 cd Coursework
 mkdir assignments
@@ -11,4 +13,19 @@ touch assignment2.txt
 echo "This is assignment 1." > assignment1.txt
 echo "This is assignment 2." > assignment2.txt
 
-cp assignment2.txt ../reports
+cp assignment1.txt ../reports
+mv assignment2.txt renamed_assignment.txt
+mv renamed_assignment.txt ../reports
+
+cd ..
+mkdir exams
+cd exams
+touch exam1.txt 
+touch exam2.txt
+cd ..
+cp -r exams assignments
+cp -r exams reports
+
+rm -r exams
+ls -l > directory_info.txt
+diff reports/assignment1.txt reports/renamed_assignment.txt > content_comparison.txt
